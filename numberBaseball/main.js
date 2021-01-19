3e3a6f99391b6545b99e009cd8b13dd396364bd4
wonhn1 <wonhn1@gmail.com>
2021-01-19 23:37:18 +0900

    webpack

---
+++ numberBaseball/main.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+import numberBaseball from './numberBaseball';
+
+new Vue(numberBaseball).$mount('#root');
---
+++ numberBaseball/numberBaseball.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{result}}</h1>
+        <input ref="answer" maxlength="4" v-model="value"
+        <button v-on:click="onSubmitBtn">버튼</button>
+        
+        <div>시도: {{}}</div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                value: '',
+                result: '',
+            }
+        },
+        methods: {
+            onSubmitBtn(e) {
+                e.preventdefault();
+            }
+        }
+    }
+</script>
+<style>
+
+</style>
