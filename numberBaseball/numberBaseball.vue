6077597e08d7c741fc6ee08eaeb2c7324d6aaac3
wonhn1 <wonhn1@gmail.com>
2021-01-20 22:16:16 +0900

    숫자야구

--- numberBaseball/numberBaseball.vue
+++ numberBaseball/numberBaseball.vue
@@ -1,23 +1,35 @@
 <template>
     <div>
         <h1>{{result}}</h1>
-        <input ref="answer" maxlength="4" v-model="value"
-        <button v-on:click="onSubmitBtn">버튼</button>
+        <input ref="answer" maxlength="4" v-model="value" id="input"/>
+        <button v-on:click="onSubmitBtn()">버튼</button>
         
-        <div>시도: {{}}</div>
+        <div>시도: {{ tries.length }}</div>
+        <ul>
+            <li v-for="t in tries">
+                <div>{{t.key}}</div>
+                <div>{{t.result}}</div>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
     export default {
         data() {
             return {
+                tries: [],
                 value: '',
                 result: '',
             }
         },
         methods: {
-            onSubmitBtn(e) {
-                e.preventdefault();
+            onSubmitBtn: function() {
+                this.tries.push({
+                    key: this.value,
+                    result: '홈런'
+                });
+                this.value = '';
+                this.$refs.answer.focus();
             }
         }
     }
